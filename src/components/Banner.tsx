0be0fe7201faf147131e7716cf73109c73cfd687
Repo-Rani <ranimiss968@gmail.com>
2025-01-12 02:51:09 -0800

    update

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -33,27 +33,27 @@ const Banner = () => {
       
         data-aos="fade-right"
         data-aos-delay="200"
-      className="relative left-[15px] top-[-440px] sm:left-[20px] sm:top-[-430px]  xxl:left-[230px] lg:left-[110px]  md:left-[100px] md:top-[-410px] xxxl:left-[220px] xxxl:top-[-480px] xxl:top-[-460px] xl:top-[-400px] xl:left-[240px] lg:top-[-420px] tracking-wide font-lato text-[12px] xsm:text-[14px] md:text-[16px] text-white">
+      className="relative left-[15px] top-[-460px] sm:left-[20px] sm:top-[-430px]  xxl:left-[230px] lg:left-[110px]  md:left-[100px] md:top-[-410px] xxxl:left-[220px] xxxl:top-[-480px] xxl:top-[-460px] xl:top-[-400px] xl:left-[240px] lg:top-[-420px] tracking-wide font-lato text-[12px] xsm:text-[14px] md:text-[16px] text-white">
         NEW RELEASE
       </span>
       <h1
        data-aos="fade-right"
         data-aos-delay="200"
-      className=" relative md:w-[300px] left-[-150px] sm:left-[-145px] top-[-440px] sm:top-[-430px] md:top-[-400px] xl:top-[-390px] xxl:top-[-445px] xxl:left-[50px] lg:left-[-60px]  lg:top-[-410px] md:left-[-70px] xl:left-[70px] tracking-normal text-white text-[28px] xsm:text-[32px] md:text-[48px] lg:text-[65px] xxxl:left-[50px] xxxl:top-[-470px] ml-[165px] font-playfair font-bold lg:leading-[80px]">
+      className=" relative md:w-[300px] left-[-150px] sm:left-[-145px] top-[-640px] sm:top-[-430px] md:top-[-400px] xl:top-[-390px] xxl:top-[-445px] xxl:left-[50px] lg:left-[-60px]  lg:top-[-410px] md:left-[-70px] xl:left-[70px] tracking-normal text-white text-[28px] xsm:text-[32px] md:text-[48px] lg:text-[65px] xxxl:left-[50px] xxxl:top-[-470px] ml-[165px] font-playfair font-bold lg:leading-[80px]">
         Metallics <br />
         <span className=" font-normal italic">Shine</span> on{" "}
       </h1>
       <p 
        data-aos="fade-right"
         data-aos-delay="300"
-      className="md:text-[16px] sm:text-[12px] text-[10px] lg:text-[18px] font-lato text-white relative md:left-[95px] lg:left-[105px] xxxl:left-[215px] xxxl:top-[-450px] xl:left-[235px] xxl:top-[-420px] xl:top-[-375px] md:top-[-390px] lg:top-[-390px] lg:leading-[30px] xxl:left-[210px] md:w-[280px] lg:w-[305px] sm:w-[178px] w-[160px] h-[84px] left-[15px] sm:left-[20px] top-[-435px] sm:top-[-425px] ">
+      className="md:text-[16px] sm:text-[12px] text-[10px] lg:text-[18px] font-lato text-white relative md:left-[95px] lg:left-[105px] xxxl:left-[215px] xxxl:top-[-450px] xl:left-[235px] xxl:top-[-420px] xl:top-[-375px] md:top-[-390px] lg:top-[-390px] lg:leading-[30px] xxl:left-[210px] md:w-[280px] lg:w-[305px] sm:w-[178px] w-[160px] h-[84px] left-[15px] sm:left-[20px] top-[-635px] sm:top-[-425px] ">
         Get to know our new eyeshadow palettes with a glossy finish, smooth
         lightweight feel and 10 hour stay-on.
       </p>
       <button
        data-aos="fade-right"
         data-aos-delay="300"
-      className="md:w-[130px] md:h-[45px] w-[65px] h-[25px] relative left-[20px]  sm:left-[20px] sm:top-[-440px] top-[-455px] md:left-[95px] xxxl:left-[220px] xxxl:top-[-420px] xxl:left-[210px] xxl:top-[-380px] xl:top-[-345px] xl:left-[235px] md:text-[18px] text-[12px]  lg:left-[108px]  md:top-[-380px] lg:top-[-360px] hover:bg-white hover:text-[#735555] hover:scale-105 duration-300 transition-all ease-in-out border-[2px] border-white  bg-transparent text-white rounded-sm">
+      className="md:w-[130px] md:h-[45px] w-[65px] h-[25px] relative left-[20px]  sm:left-[20px] sm:top-[-440px] top-[-655px] md:left-[95px] xxxl:left-[220px] xxxl:top-[-420px] xxl:left-[210px] xxl:top-[-380px] xl:top-[-345px] xl:left-[235px] md:text-[18px] text-[12px]  lg:left-[108px]  md:top-[-380px] lg:top-[-360px] hover:bg-white hover:text-[#735555] hover:scale-105 duration-300 transition-all ease-in-out border-[2px] border-white  bg-transparent text-white rounded-sm">
         <Link href="/shopall"> Shop</Link>
       </button>
     </>
