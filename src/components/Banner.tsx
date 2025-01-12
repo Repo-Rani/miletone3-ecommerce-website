8724d582be5eb81ea22d34fe90fa5df5e337c3be
Repo-Rani <ranimiss968@gmail.com>
2025-01-12 00:40:36 -0800

    update

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const Banner = () => {
   return (
     <>
-      <div className="relative  md:w-full h-[75vh] md:h-screen xxl:top-[115px] xl:top-[108px]  md:top-[105px] ">
+      <div className="relative  md:w-full h-[70vh] md:h-screen xxl:top-[115px] xl:top-[108px]  md:top-[105px] ">
         <div className="hidden md:block">
           <Image
             src="/hero-image.avif"
@@ -53,7 +53,7 @@ const Banner = () => {
       <button
        data-aos="fade-right"
         data-aos-delay="300"
-      className="md:w-[130px] md:h-[45px] w-[65px] h-[25px] relative left-[15px]  sm:left-[20px] sm:top-[-440px] top-[-460px] md:left-[95px] xxxl:left-[220px] xxxl:top-[-420px] xxl:left-[210px] xxl:top-[-380px] xl:top-[-345px] xl:left-[235px] md:text-[18px] text-[12px]  lg:left-[108px]  md:top-[-380px] lg:top-[-360px] hover:bg-white hover:text-[#735555] hover:scale-105 duration-300 transition-all ease-in-out border-[2px] border-white  bg-transparent text-white rounded-sm">
+      className="md:w-[130px] md:h-[45px] w-[65px] h-[25px] relative left-[15px]  sm:left-[20px] sm:top-[-440px] top-[-40px] md:left-[95px] xxxl:left-[220px] xxxl:top-[-420px] xxl:left-[210px] xxl:top-[-380px] xl:top-[-345px] xl:left-[235px] md:text-[18px] text-[12px]  lg:left-[108px]  md:top-[-380px] lg:top-[-360px] hover:bg-white hover:text-[#735555] hover:scale-105 duration-300 transition-all ease-in-out border-[2px] border-white  bg-transparent text-white rounded-sm">
         <Link href="/shopall"> Shop</Link>
       </button>
     </>
